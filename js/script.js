809d6222a389157f02dd62a5b097370ba95e4e4e
Krzysztof Gumuła <k.gumula@icloud.com>
2020-02-04 21:38:44 +0100

    add game score script

--- js/script.js
+++ js/script.js
@@ -29,3 +29,31 @@ if(playerInput == '1'){
     playerMove = 'nozyce';
 }
 printMessage('Gracz: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Gracz wygrywa!');
+  } else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'papier' && playerMove == 'nozyce'){
+    printMessage('Gracz wygrywa!');
+  } else if (computerMove == 'nozyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'nozyce' && playerMove == 'kamień'){
+    printMessage('Gracz wygrywa!');
+  } else if (computerMove == 'kamień' && playerMove == 'nozyce'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'nozyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  } else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  } else if (computerMove == 'nozyce' && playerMove == 'nozyce'){
+    printMessage('remis!');
+  } else if (computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage('Spróbuj ponownie!');
+  } else if (computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('Spróbuj ponownie!');
+  } else if (computerMove == 'nozyce' && playerMove == 'nieznany ruch'){
+    printMessage('Spróbuj ponownie!');
+  }
