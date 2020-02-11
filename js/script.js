23f45a42ef2bc3d10062e245a7b7e5a369b9fa23
Krzysztof Gumuła <k.gumula@icloud.com>
2020-02-11 20:26:33 +0100

    add play again

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
   let playerPoints = 0;
   let numberOfRounds = prompt('Ile rund ma trwać rozgrywka?');
 
-  function playGame(playerInput) {
+  const playGame = function (playerInput) {
     clearMessages();
 
-    function getMoveName(argMoveId) {
+    const getMoveName = function (argMoveId) {
       if (argMoveId == 1) {
         return 'kamień';
       } else if (argMoveId == 2) {
@@ -20,7 +20,7 @@
       }
     }
 
-    function displayResult(argComputerMove, argPlayerMove) {
+    const displayResult = function (argComputerMove, argPlayerMove) {
       printMessage('Komputer: ' + argComputerMove + ' || ' + 'Gracz: ' + argPlayerMove);
 
       if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
@@ -56,12 +56,13 @@
     let getResult = displayResult(computerMove, playerMove);
   }
 
-  let showWinner = function () {
+  const showWinner = function () {
     if (computerPoints == numberOfRounds || playerPoints == numberOfRounds) {
       document.getElementById('play-rock').style.display = 'none';
       document.getElementById('play-paper').style.display = 'none';
       document.getElementById('play-scissors').style.display = 'none';
       document.getElementById('result').style.display = 'none';
+      document.getElementById('play-again').style.display = 'inline';
     }
 
     if (computerPoints == numberOfRounds && computerPoints > playerPoints) {
@@ -70,16 +71,21 @@
       printMessage('Wygrywa gracz!')
     }
   }
+  const reset = function () {
+    
+  }
 
-  let playRock = document.getElementById('play-rock');
-  let playPaper = document.getElementById('play-paper');
-  let playScissors = document.getElementById('play-scissors');
-  let launchGame = document.getElementById('launch-game');
+  const playRock = document.getElementById('play-rock');
+  const playPaper = document.getElementById('play-paper');
+  const playScissors = document.getElementById('play-scissors');
+  const launchGame = document.getElementById('launch-game');
+  const playAgain = document.getElementById('play-again');
 
   document.getElementById('play-rock').style.display = 'none';
   document.getElementById('play-paper').style.display = 'none';
   document.getElementById('play-scissors').style.display = 'none';
   document.getElementById('result').style.display = 'none';
+  document.getElementById('play-again').style.display = 'none';
 
   launchGame.addEventListener('click', function () {
     document.getElementById('play-rock').style.display = 'inline';
@@ -87,6 +93,16 @@
     document.getElementById('play-scissors').style.display = 'inline';
     document.getElementById('result').style.display = 'inline';
     document.getElementById('launch-game').style.display = 'none';
+    document.getElementById('play-again').style.display = 'none';
+  });
+
+  playAgain.addEventListener('click', function () {
+    document.getElementById('play-rock').style.display = 'none';
+    document.getElementById('play-paper').style.display = 'none';
+    document.getElementById('play-scissors').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
+    document.getElementById('launch-game').style.display = 'inline';
+    document.getElementById('play-again').style.display = 'none';
   });
 
   playRock.addEventListener('click', function () {
@@ -108,6 +124,10 @@
     clearResult();
     printResult('Komputer ' + computerPoints + ' : ' + playerPoints + ' Gracz');
     showWinner();
+  });
+  playAgain.addEventListener('click', function () {
+    clearMessages();
+    clearResult();
   });
 
 }
