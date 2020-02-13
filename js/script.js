b019f6ffadb58168afa063e15bbdbd5da4b6db49
Krzysztof Gumuła <k.gumula@icloud.com>
2020-02-13 20:34:21 +0100

    function improvement

--- js/script.js
+++ js/script.js
@@ -2,55 +2,52 @@
 
   let computerPoints = 0;
   let playerPoints = 0;
-  let numberOfRounds = prompt('Ile rund ma trwać rozgrywka?');
+  let numberOfRounds = 0;
+
+  const displayResult = function (argComputerMove, argPlayerMove) {
+    printMessage('Komputer: ' + argComputerMove + ' || ' + 'Gracz: ' + argPlayerMove);
+
+    if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+      printMessage('Ty wygrywasz!');
+      playerPoints++;
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+      printMessage('Przegrywasz!');
+      computerPoints++;
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+      printMessage('Przegrywasz!');
+      computerPoints++;
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+      printMessage('Ty wygrywasz!');
+      playerPoints++;
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+      printMessage('Ty wygrywasz!');
+      playerPoints++;
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+      printMessage('Przegrywasz!');
+      computerPoints++;
+    } else if (argComputerMove == argPlayerMove) {
+      printMessage('Remis!');
+    }
+  }
+
+  const getMoveName = function (argMoveId) {
+    if (argMoveId == 1) {
+      return 'kamień';
+    } else if (argMoveId == 2) {
+      return 'papier';
+    } else if (argMoveId == 3) {
+      return 'nożyce'
+    } else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+      return 'nieznany ruch';
+    }
+  }
 
   const playGame = function (playerInput) {
     clearMessages();
-
-    const getMoveName = function (argMoveId) {
-      if (argMoveId == 1) {
-        return 'kamień';
-      } else if (argMoveId == 2) {
-        return 'papier';
-      } else if (argMoveId == 3) {
-        return 'nożyce'
-      } else {
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
-      }
-    }
-
-    const displayResult = function (argComputerMove, argPlayerMove) {
-      printMessage('Komputer: ' + argComputerMove + ' || ' + 'Gracz: ' + argPlayerMove);
-
-      if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-        playerPoints++;
-      } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-        printMessage('Przegrywasz!');
-        computerPoints++;
-      } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-        printMessage('Przegrywasz!');
-        computerPoints++;
-      } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
-        playerPoints++;
-      } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-        playerPoints++;
-      } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-        printMessage('Przegrywasz!');
-        computerPoints++;
-      } else if (argComputerMove == argPlayerMove) {
-        printMessage('Remis!');
-      }
-
-    }
-
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
     let computerMove = getMoveName(randomNumber);
-
     let playerMove = getMoveName(playerInput);
     console.log('Gracz wpisał: ' + playerInput);
     let getResult = displayResult(computerMove, playerMove);
@@ -71,9 +68,6 @@
       printMessage('Wygrywa gracz!')
     }
   }
-  const reset = function () {
-    
-  }
 
   const playRock = document.getElementById('play-rock');
   const playPaper = document.getElementById('play-paper');
@@ -88,6 +82,7 @@
   document.getElementById('play-again').style.display = 'none';
 
   launchGame.addEventListener('click', function () {
+    numberOfRounds = prompt('Ile rund ma trwać rozgrywka?');
     document.getElementById('play-rock').style.display = 'inline';
     document.getElementById('play-paper').style.display = 'inline';
     document.getElementById('play-scissors').style.display = 'inline';
@@ -103,6 +98,8 @@
     document.getElementById('result').style.display = 'none';
     document.getElementById('launch-game').style.display = 'inline';
     document.getElementById('play-again').style.display = 'none';
+    clearMessages();
+    clearResult();
   });
 
   playRock.addEventListener('click', function () {
@@ -125,9 +122,4 @@
     printResult('Komputer ' + computerPoints + ' : ' + playerPoints + ' Gracz');
     showWinner();
   });
-  playAgain.addEventListener('click', function () {
-    clearMessages();
-    clearResult();
-  });
-
 }
